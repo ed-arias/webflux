06b8c84bc47b921ed8bcd96c5b337f07bcd270e1
Eduardo Arias <ing.ed.arias@gmail.com>
2020-08-15 22:49:38 -0300

    Implementing the service and service provider

---
+++ src/main/java/com/apirest/webflux/services/PlaylistServiceProvider.java
@@ -0,0 +1,33 @@
+package com.apirest.webflux.services;
+
+import com.apirest.webflux.documents.Playlist;
+import com.apirest.webflux.repositories.PlaylistRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class PlaylistServiceProvider implements PlaylistService {
+
+    @Autowired
+    PlaylistRepository playlistRepository;
+
+    @Override
+    public Flux<Playlist> findAll() {
+        return playlistRepository.findAll();
+    }
+
+    @Override
+    public Mono<Playlist> findById(String id) {
+        return playlistRepository.findById(id);
+    }
+
+    @Override
+    public Mono<Playlist> save(Playlist playlist) {
+        return playlistRepository.save(playlist);
+    }
+    
+}
